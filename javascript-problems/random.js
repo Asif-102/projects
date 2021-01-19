243e880a7d81f13118d7676f7642032683801655
Asif2535 <aiqbal517@gmail.com>
2021-01-20 00:31:40 +0600

    add random programm

---
+++ javascript-problems/random.js
@@ -0,0 +1,13 @@
+var num = 2.212458;
+var result1 = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+for (var i = 0; i < 10; i++) {
+    var randomNum = Math.random() * 10;
+    var output = Math.round(randomNum);
+    console.log(output);
+}
